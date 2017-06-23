8ce4b207683b819029e2c0bc826a742c8585ddd8
Zhang Dongya <fortitude.zhang@gmail.com>
2017-06-23 22:23:24 +0800

    revert batch

--- netlib.c
+++ netlib.c
@@ -315,7 +315,8 @@ int kcpoudp_session(struct vtun_host *host)
      // KCP KLUDGE HERE.
      {
          host->kcp = ikcp_create(kcpudp_generate_conn_id_by_host(host->host), (void *)host);
-         ikcp_nodelay(host->kcp, 1, 10, 2, 1); // enable fast fast mode!!!
+         //         ikcp_nodelay(host->kcp, 1, 10, 2, 1); // enable fast fast mode!!!
+         ikcp_nodelay(host->kcp, 0, 40, 0, 0); // normal mode.
          host->kcp->rx_minrto = 10; // detect drop asap.
          host->kcp->fastresend = 1; // detect drop asap.
          // refer to kcpodup_read static_buf size if you want to change that.
